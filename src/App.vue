f00d2329e97a72fa6d1762355f42153e2ca8bd7c
Codesmy <akulab2b@gmail.com>
2022-12-16 00:11:36 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import VBtn from './components/v-btn.vue'
+</script>
+
+<template>
+  <VBtn 
+  buttonText="Отправить письмо" 
+  />
+</template>
+
+
+<script>
+export default {
+  name: "App",
+  components: {
+    VBtn,
+  }
+}
+</script>
---
+++ src/components/v-btn.vue
@@ -0,0 +1,39 @@
+<script setup>
+import IconArrow1 from "./icons/IconArrow1.vue";
+</script>
+
+
+<template>
+  <div 
+  class="v-btn"
+  
+  >
+    <span>{{ buttonText }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonText: {
+      type: String,
+      default: () => "",
+    },
+    
+  }
+  
+
+};
+</script>
+
+<style lang="sass">
+.v-btn
+  border-radius: 20px
+  padding: 14px 46px
+  color: #fff
+  background: #000
+  cursor: pointer
+  font-size: 18px
+  text-transform: uppercase
+  
+</style>
